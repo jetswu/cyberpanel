e4224f419d70f5892ffbb87d133dbdab3bca2bbd
wacincong <alanhhidayat92@gmail.com>
2025-11-19 07:24:47 +0700

    Create installer.sh
    Ngecit instaler

---
+++ installer.sh
@@ -0,0 +1,7 @@
+rpm --import https://repo.almalinux.org/almalinux/RPM-GPG-KEY-AlmaLinux && \
+dnf update -y && \
+dnf install epel-release -y && \
+dnf upgrade -y && \
+yum install screen -y && \
+screen -S cyberpanel -dm sh <(curl https://cyberpanel.net/install.sh || wget -O - https://cyberpanel.net/install.sh) && \
+echo "Instalasi CyberPanel dimulai di sesi screen 'cyberpanel'. Untuk melihat progres, jalankan: screen -r cyberpanel"

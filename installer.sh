bdcaf477aaa0399e79b3e9f081ff199c2adc1146
wacincong <alanhhidayat92@gmail.com>
2026-01-09 18:47:13 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-dnf update -y
-dnf install epel-release -y
-wget -O cyberpanel_install.sh https://cyberpanel.net/install.sh
-sh cyberpanel_install.sh
+dnf update -y && dnf install epel-release -y && wget -O installer.sh https://cyberpanel.net/install.sh && bash installer.sh

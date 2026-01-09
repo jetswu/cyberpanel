076632d7e97bd86b6eb2c06314739123b1da81f7
wacincong <alanhhidayat92@gmail.com>
2026-01-09 18:52:13 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 dnf update -y
 dnf install epel-release -y
-wget -O cyberpanel_install.sh https://cyberpanel.net/install.sh
-# Gunakan exec agar input keyboard diteruskan dengan benar
-exec sh cyberpanel_install.sh
+wget -O - https://cyberpanel.net/install.sh
